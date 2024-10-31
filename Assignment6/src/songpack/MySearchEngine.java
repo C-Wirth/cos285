29688b501c121c8d1e4157b843302871c5732360
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2024-10-31 17:57:39 -0400

    lab 6 completed

--- Assignment6/src/songpack/MySearchEngine.java
+++ Assignment6/src/songpack/MySearchEngine.java
@@ -8,17 +8,20 @@
 package songpack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class MySearchEngine {
 
     TreeMap<Song, TreeMap<String, Double>> tf;
-
-    ArrayList<Song> songs;
-
+    TreeMap<String, Double> idf;
+    TreeMap<Song, Double> avglength;
 
     public MySearchEngine(ArrayList<Song> songs){
-        this.songs= songs;
+        calculateTF(songs);
+        calculateIDF(songs);
+        calculateAvgLength(songs);
     }
 
     /**
@@ -27,19 +30,66 @@ public class MySearchEngine {
      * and the Double is the the count of the lyric
      * 
      */
-    public void calculateTF(){
+    public void calculateTF(ArrayList<Song>  songs){
 
         for(Song song : songs){
 
             TreeMap<String, Double> lyricCount = new TreeMap<>();
 
-            String[] lyrics = song.getLyrics().split("\\\s"); // splitting function got from stackoverflow on 10-30-2024 
-                                                                    // https://stackoverflow.com/questions/7899525/how-to-split-a-string-by-space
+            String[] lyrics = song.getLyrics().split("\\s+");
+
             for(String lyric : lyrics){   
                 lyricCount.put(lyric, lyricCount.getOrDefault(lyric, 0.0) + 1.0);
             }
            tf.put(song, lyricCount);
         }
+
+        int numWords = lyrics.size();
+        for(lyric : lyrics){ //i need the ratio - update the tfTree value
+
+        }
     }
 
+    // implement IOT 
+    public void calculateIDF(ArrayList<Song> songs){
+
+        for(Song song : songs){
+
+            String[] words = song.getLyrics().split(" ");
+
+            TreeSet<String> uniqueWords = new TreeSet<>(Arrays.asList(words));
+
+            for(String word : uniqueWords){
+
+                idf.put(word, idf.getOrDefault(word, 0.0)+1);
+            }
+        }
+
+        for(String lyric : idf.keySet()){
+
+            double nX = idf.get(lyric);
+
+            double idfVal = (songs.size()- (nX + 0.5)/(nX + 0.5)) + 1;
+            
+            idfVal = Math.log(idfVal);
+
+            idf.put(lyric, idfVal);
+
+        }
+    }
+
+    public void calculateAvgLength(ArrayList<Song> songs){
+
+        double summation = 0.0;
+        for(Song song : songs){
+
+            summation += song.getLyrics().split("\\s+").length;
+        }
+
+        double avg = summation/songs.size();
+        for(Song song : songs){
+            avglength.put(song, avglength.get(song)/avg);
+
+        }
+    }
 }
