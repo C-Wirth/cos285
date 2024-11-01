77e268860d8ed251edaf16a482ac3743d3be741b
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2024-11-01 01:16:25 -0400

    broken search engine implemented.

--- Assignment6/src/songpack/MySearchEngine.java
+++ Assignment6/src/songpack/MySearchEngine.java
@@ -9,18 +9,20 @@ package songpack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-public class MySearchEngine {
+public final class MySearchEngine {
 
     public static double CONSTANT_B = 0.75;
     public static double CONSTANT_K_1 = 1.2;
 
-
-    TreeMap<Song, TreeMap<String, Double>> tf;//term frequency
-    TreeMap<String, Double> idf; //Inverse Document Frequency
-    TreeMap<Song, Double> avglength; //avearge length of each song
+    TreeMap<Song, TreeMap<String, Double>> tf = new TreeMap<>();;//term frequency
+    TreeMap<String, Double> idf = new TreeMap<>();; //Inverse Document Frequency
+    TreeMap<Song, Double> avglength = new TreeMap<>();; //avearge length of each song
     double avgSongLength;
 
     /**
@@ -48,7 +50,7 @@ public class MySearchEngine {
 
             TreeMap<String, Double> lyricCount = new TreeMap<>();
 
-            String[] lyrics = song.getLyrics().split("\\s+");
+            String[] lyrics = song.getLyrics().toLowerCase().split("\\s+");
 
             for(String lyric : lyrics){   
                 lyricCount.put(lyric, lyricCount.getOrDefault(lyric, 0.0) + 1.0);
@@ -63,7 +65,7 @@ public class MySearchEngine {
     /**
      * This method populates a MySearchEngine's TreeMap<String, Double> idf;
      * The keys are a set of Strings which repesent every lyric found from the ArrayList of Songs passed into the Constructor
-     * The values is the idf value for each lyric in the entire set of songs
+     * Each value is the idf value for each lyric in the entire set of songs
      * @param songs
      */
     public void calculateIDF(ArrayList<Song> songs){
@@ -85,8 +87,9 @@ public class MySearchEngine {
             double nX = idf.get(lyric);
 
             double idfVal = (songs.size()- (nX + 0.5)/(nX + 0.5)) + 1;
-            
             idfVal = Math.log(idfVal);
+
+            // double idfVal = Math.log((double) songs.size() / (1 + nX));
 
             idf.put(lyric, idfVal);
 
@@ -104,7 +107,9 @@ public class MySearchEngine {
 
         double summation = 0.0;
         for(Song song : songs){
-            summation += song.getLyrics().split("\\s+").length;
+            int songLength = song.getLyrics().split("\\s+").length;
+            summation += songLength;
+            avglength.put(song, (double) songLength);
         }
 
         this.avgSongLength = summation/songs.size();
@@ -114,18 +119,28 @@ public class MySearchEngine {
         }
     }
 
-    
+    /**
+     * This method calculates the relevance score of a query with respect to a Song
+     * @param song the Song to find a relevance score
+     * @param query user's query
+     * @return the calculated relevance score
+     */
     public double calculateRelevance(Song song, String query){
 
-        String[] queryArray = query.split(" ");
+        String[] queryArray = query.split("\\s+");
 
         double score = 0.0;
 
         for(String word : queryArray){ //relevance equation
 
-            double idfVal = idf.get(word);
+            word = word.toLowerCase();
 
-            double tfVal = tf.get(song).get(word);
+            double idfVal = idf.getOrDefault(word, 0.0);
+
+            double tfVal = 0;
+
+            if(tf.get(song) != null && tf.get(song).get(word) != null)
+                tfVal = tf.get(song).get(word);
 
             double songLength = song.getLyrics().split("\\s+").length;
 
@@ -133,9 +148,69 @@ public class MySearchEngine {
            double denominator =tfVal + (CONSTANT_K_1 * ((1.0-CONSTANT_B)+(CONSTANT_B*(songLength/avgSongLength))));
 
            score+=(numerator/denominator);
+
         }
-
-
         return score;
     } 
+
+    /**
+     * serach method takes the query and prints the top-5 songs with the highest relevacne score
+     * @param query
+     */
+    public void search(String query){
+
+        TreeMap<Song, Double> relevanceScores = new TreeMap<>();
+
+        for(Song song : tf.keySet()){ //look at all songs in the MySearchEngine
+            relevanceScores.put(song,calculateRelevance(song, query));
+        }
+  
+        List<Map.Entry<Song, Double>> sortedOutput = sortedByValue(relevanceScores, 5);
+
+        printSearchResults(query, sortedOutput);
+    }
+
+    /**
+     * helper method fpr search method
+     * 
+     * This method returns a sorted list of top k results as a list of entries <Song,Double>
+     * @param treeMap
+     * @param topK
+     * @return A list of Maps with songs and their relevance value
+     */
+    private List<Map.Entry<Song, Double>> sortedByValue(TreeMap<Song, Double> treeMap, int topK){
+        
+        // Convert to list of Map.Entry
+        List<Map.Entry<Song, Double>> list = new ArrayList<>(treeMap.entrySet());
+        
+        // Sort the list based on values
+        Collections.sort(list, (Map.Entry<Song, Double> o1, Map.Entry<Song, Double> o2) 
+        -> o2.getValue().compareTo(o1.getValue()));
+    
+        int counter = 0;
+        List<Map.Entry<Song, Double>> results = new ArrayList<>();
+        
+        while(counter<topK){
+            results.add(Map.entry(list.get(counter).getKey(),list.get(counter).getValue()));
+            counter+=1;
+        }
+    
+    return results;
+    }
+
+    /**
+     * Helper method to format and print results from a query
+     * @param query the user's query
+     * @param results the ordered results
+     */
+    private void printSearchResults(String query, List<Map.Entry<Song, Double>> results){
+        System.out.println("\nResults for "+ query);
+        int rank = 1;
+        for(Map.Entry<Song, Double> entry: results){
+        System.out.println(rank+": " + entry.getKey().getTitle() + " by " + entry.getKey().getArtist() + "\t"
+        + entry.getValue());
+        rank+=1;
+        }
+        }
+
 }
--- Assignment6/src/songpack/program6.java
+++ Assignment6/src/songpack/program6.java
@@ -1,5 +1,7 @@
 package songpack;
 
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Colby Wirth
@@ -8,6 +10,48 @@ package songpack;
  * program 6
  */
 public class program6 {
+
+    public static final String QUERY0 = "back in black";
+    public static final String QUERY1 = "we are the champions";
+    public static final String QUERY2 = "i will always love you";
+    public static final String QUERY3 = "walking on sunshine";
+    public static final String QUERY4 = "dancing in the rain";
+    public static final String QUERY5 = "put your hands in the jupitersky";
+        
+    public static String filePath="src/song_lyrics.tsv";
+    public static final String GENRE = "rock";
+
+    public static MySearchEngine engine ;
+    
+        public static void main(String[] args) throws IOException{
+    
+            if(args.length != 0){
+                filePath = args[0];
+            }
+    
+            ArrayList<Song> songs = MyDataReader.readFileToArrayList(filePath, GENRE);
+    
+            //building the index
+            long startTime = System.currentTimeMillis();
+            engine = new MySearchEngine(songs);
+            long endtime = System.currentTimeMillis() - startTime;
+            System.out.println("\n" + endtime + " milliseconds to build the index");
+    
+            //execute queries
+            queryMaker(QUERY0);
+            queryMaker(QUERY1);
+            queryMaker(QUERY2);
+            queryMaker(QUERY3);
+            queryMaker(QUERY4);
+            queryMaker(QUERY5);
+        }
+
+    private static void queryMaker(String query){
+        long startTime = System.currentTimeMillis();
+        engine.search(query);
+        long endtime = System.currentTimeMillis() - startTime;
+        System.out.println(endtime + " milliseconds to search for " + query);
+    }
 
     
 
