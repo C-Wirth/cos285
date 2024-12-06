07e4c3817fc218d8a448f4a6126a99db713787e2
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2024-12-05 20:37:06 -0500

    lab 9 completed

--- Assignment10/src/MySorts.java
+++ Assignment10/src/MySorts.java
@@ -16,6 +16,7 @@ public class MySorts {
         int[] arrayForInsertionSort = randomArray.clone();
         int[] arrayForMergeSort = randomArray.clone();
 
+        //insertion sort
         long startTime = System.nanoTime();
         insertionSort(arrayForInsertionSort);
         long endTime = System.nanoTime();
@@ -23,6 +24,7 @@ public class MySorts {
         System.out.println("Insertion Sort time: " + insertionSortTime + " nanoseconds");
         printArray(arrayForInsertionSort);
 
+        //merge sort
         startTime = System.nanoTime();
         mergeSort(arrayForMergeSort);
         endTime = System.nanoTime();
@@ -34,7 +36,7 @@ public class MySorts {
     public static void insertionSort(int[] input){
         for(int i =1; i < input.length; i++){
             for(int j=i; j>0 && (input[j-1]>input[j]); j--){
-                int temp = input[i];
+                int temp = input[j];
                 input[j]=input[j-1];
                 input[j-1]=temp;
             }
@@ -50,12 +52,44 @@ public class MySorts {
         int[] right = new int[array.length-mid];
 
         System.arraycopy(array, 0, left, 0, mid);
-        System.arraycopy(array, mid, right, array.length-mid, mid);
+        System.arraycopy(array, mid, right, 0, array.length-mid);
 
         mergeSort(left);
         mergeSort(right);
 
         merge(array,left,right);
+    }
+
+    private static void merge(int[] array, int[] left, int[] right){
+
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int i = 0;
+
+        while(leftPointer < left.length || rightPointer < right.length){  //**** WE HAVE VALUES IN LEFT AND RIGHT)
+
+            if(leftPointer >= left.length){ //edge case - left is at end, add right value
+                array[i] = right[rightPointer];
+                rightPointer++;
+                i++;
+            }
+            else if (rightPointer >= right.length){ //edge case - right is at end, add left value
+                array[i] = left[leftPointer];
+                leftPointer++;
+                i++;
+            }
+
+            else if (left[leftPointer] < right[rightPointer]){
+                array[i] = left[leftPointer];
+                leftPointer++;
+                i++;
+            }
+            else{
+                array[i] = right[rightPointer];
+                rightPointer++;
+                i++;
+            }
+            }
     }
 
     public static void printArray(int[] input){
